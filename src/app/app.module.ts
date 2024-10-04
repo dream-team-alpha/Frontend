135ada8d1275c20c17d76fef653d253f4e1feafe
dream-team-alpha <dev@dreamreflectionmedia.com>
2024-10-04 11:38:41 +0530

    created folder structure, model,helper,components,gaurds, services and installed angular materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './components/auth/admin/sign-up/sign-up.component';
+import { LoginComponent } from './components/auth/admin/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
