bccfdd5107438afe1af0e6b8a3f07f820628dcfa
dream-team-alpha <dev@dreamreflectionmedia.com>
2024-10-15 17:49:19 +0530

    made sidebar dynamic

--- src/app/components/dashboard/sidebar/sidebar.component.ts
+++ src/app/components/dashboard/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { User } from 'src/app/services/user/user.service';
 
 @Component({
@@ -7,11 +8,31 @@ import { User } from 'src/app/services/user/user.service';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   @Output() userSelected = new EventEmitter<User>(); 
   searchText: string = '';
 
-  constructor(private router: Router) {}
+  adminId: string = localStorage.getItem('adminId') || '';
+  adminName: string = ''; 
+  adminAvatar: string = ''; // Variable to hold the avatar URL
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.getAdminProfile(this.adminId);
+  }
+
+  private getAdminProfile(adminId: string): void {
+    this.authService.getAdminProfile(adminId).subscribe(
+      (profile) => {
+        this.adminName = `${profile.firstName} ${profile.lastName}`; // Set the admin's name
+        this.adminAvatar = `http://localhost:5000${profile.avatar}`; // Construct avatar URL
+      },
+      (error) => {
+        console.error('Error fetching admin profile:', error);
+      }
+    );
+  }
 
   onUserSelected(user: User): void {
     this.userSelected.emit(user);
